0af83c64c360ca7b32c87fac310c3f086cbb6d00
Vicenciomf <vxmartinez@ug.uchile.cl>
2022-10-26 21:59:38 -0300

    TIntro a Express

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const Contenedor = require('./Contenedor.js');
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${server.address().port}`);
+});
+server.on("error", error => console.log(`Encontramos el siguiente error en el servidor: ${error}`));
+
+
+app.get('/productos', async (req, res) => {
+  const contenedor = new Contenedor('productos.txt');
+  const productos = await contenedor.getAll();
+  res.send(productos);
+});
+
+app.get('/productoRandom', async (req, res) => {
+  const contenedor = new Contenedor('productos.txt');
+  const productos = await contenedor.getAll();
+
+  const indiceAleatorio = Math.floor(Math.random() * productos.length);
+  const productoRandom = productos[indiceAleatorio];
+  res.send(productoRandom);
+});
+//Pd: ¿Cómo separo las rutas de la configuración del server? No me gusta verlo todo en un mismo archivo
